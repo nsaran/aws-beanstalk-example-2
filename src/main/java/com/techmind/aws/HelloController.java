a18510323c539c2a45243dd87aaa1864e735b57d
Natesan <saravanan.natesan@accenture.com>
2019-11-08 08:19:30 -0500

    AWS Beanstalk buildspec.yaml added runtime

--- src/main/java/com/techmind/aws/HelloController.java
+++ src/main/java/com/techmind/aws/HelloController.java
@@ -1,11 +1,13 @@
 package com.techmind.aws;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/hello")
 @RestController
 public class HelloController {
+	@GetMapping
 	public String hello() {
 		return "Hello Saravanan";
 	}
